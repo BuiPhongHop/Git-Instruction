12d0a5fcef51069c4b25a039294870afc15ddda2
Phong_Hop <hop.buikhmtk2025@hcmut.edu.vn>
2026-06-14 15:32:27 +0700

    add SecondFile and ThirdFile - min, max and check prime

---
+++ SecodeFile.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMax(int arr[], int size)
+{
+    int max = INT_MIN;
+    for(int i = 0 ; i < size ; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+int findMin(int arr[], int size)
+{
+    int min = INT_MAX;
+    for(int i = 0 ; i < size ; i++)
+    {
+        if(arr[i] < min) min = arr[i];
+    }
+    return min;
+}
---
+++ ThirdFile.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 2) return false;
+    else if(n == 2) return true;
+    else
+    {
+    	for (int i = 3; i <= sqrt(n); i += 2) {
+	        if (n % i == 0) return false;
+	    }
+	}
+    return true;
+}
