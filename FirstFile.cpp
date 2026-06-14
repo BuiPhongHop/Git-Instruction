20167839df3a60db0f4802984b3ef0956e395898
Phong_Hop <hop.buikhmtk2025@hcmut.edu.vn>
2026-06-14 15:22:23 +0700

    add FirstFile.cpp - sum, diff, product, divide

---
+++ FirstFile.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int different(int a, int b)
+{
+    return a-b;
+}
+
+int product(int a, int b)
+{
+    return a*b;
+}
+
+int divide(int a, int b)
+{
+    return a/b;
+}
